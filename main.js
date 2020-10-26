f1c031244a9db83f48ebd0a853fb11b70d3bd42a
zspFO <73490125+zspFO@users.noreply.github.com>
2020-10-26 22:50:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const name = "Tomek"
+let wiek = 20
+console.log(`Mam na imie ${name} i mam ${wiek} lat`)
+
+const imie = prompt('Podaj swoje imie: ')
+const head = document.querySelector('h1')
+console.log(head.innerHTML)
+
+head.innerHTML = `WITAJ ${imie} NA MOJEJ STRONIE!`
